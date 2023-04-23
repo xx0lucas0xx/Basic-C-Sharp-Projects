8df4c1b1766eb7e17b7f7840d9fd6110e1dfad8e
xx0lucas0xx <lucas.Rempart@gmail.com>
2023-04-22 23:47:42 -0600

    fin

--- ClassesObjects/ClassesObjects/Program.cs
+++ ClassesObjects/ClassesObjects/Program.cs
@@ -13,8 +13,9 @@ namespace ClassesObjects
         {
             //initiates empty list
             Deck deck = new Deck();
+            deck = Shuffle(deck);
             
-            //
+            //displays each card combo possible to display full deck amount of cards
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
@@ -22,6 +23,26 @@ namespace ClassesObjects
             //show deck constructor is creating a cards list and adding one card to it
             Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
+        }
+        
+        //method to shuffle the deck
+        public static Deck Shuffle(Deck deck)
+        {
+            //creates a temparary list of cards
+            List<Card> TempList = new List<Card>();
+            //randomizes the deck class
+            Random random = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                //grabs random card and puts it into a temparary deck until there are no more cards
+                int randomIndex = random.Next(0, deck.Cards.Count);
+                TempList.Add(deck.Cards[randomIndex]);
+                deck.Cards.RemoveAt(randomIndex);
+            }
+            deck.Cards = TempList;
+            return deck;
+            
         }
     }
 }
