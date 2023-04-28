0332bdc4dec6df86abd06b7e90e5961c8f9e86f8
xx0lucas0xx <lucas.Rempart@gmail.com>
2023-04-28 16:39:40 -0600

    what we got

---
+++ CallingMethods2/CallingMethods2/Numbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods2
+{
+    public class Numbers
+    {
+        public static int addition(int add)
+        {
+            return add + 2300;
+        }
+    }
+}
