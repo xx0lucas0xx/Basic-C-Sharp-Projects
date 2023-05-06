478da766bdd75fed5a0fde124a8b2bc78236fd93
xx0lucas0xx <lucas.Rempart@gmail.com>
2023-05-06 10:54:19 -0600

    fin

---
+++ CallingMethods4/CallingMethods4/Numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods4
+{
+    class Numbers
+    {
+        public void maths(int x, int y)
+        {
+            int res = x * 12;
+            Console.WriteLine(res + "\nThe second integer is: " + y);
+        }
+    }
+}
---
+++ CallingMethods4/CallingMethods4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiates the class
+            Numbers math = new Numbers();
+
+            //passing in two numbers to the maths method
+            math.maths(58, 23);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
