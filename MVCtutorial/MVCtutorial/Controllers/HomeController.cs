0f189bc59ad01e8442b47fa3b01da8f9bca70977
xx0lucas0xx <lucas.Rempart@gmail.com>
2023-08-07 20:52:15 -0600

    update

---
+++ MVCtutorial/MVCtutorial/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MVCtutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCtutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Lucas";
+            user.LastName = "English";
+            user.Age = 20;
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
