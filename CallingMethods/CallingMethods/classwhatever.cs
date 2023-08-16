ef87354ea941b31aef796df1735eef1fbf0429b8
xx0lucas0xx <lucas.Rempart@gmail.com>
2023-08-15 20:23:06 -0600

    update

--- CallingMethods/CallingMethods/classwhatever.cs
+++ CallingMethods/CallingMethods/classwhatever.cs
@@ -27,20 +27,5 @@ namespace CallingMethods
             return z * 365;
         }
 
-
-
-        ////constructor setting up default properties to class Game
-        //public Game()
-        //{
-        //    //price of game and lenght of game
-        //    Price = 60;
-        //    GameTime = 40;
-
-        //}
-
-        ////adding class property
-        //public int Price { get; set; }
-        //public int GameTime { get; set; }
-
     }
 }
